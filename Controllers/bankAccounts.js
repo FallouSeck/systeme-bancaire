d389bf99eaaeda331a2c50b761b07d47a17d6019
Fadel Seck <fadelseck92@gmail.com>
2021-04-15 18:05:57 +0200

    ajout du droit d'accès de modification de compte pour les managers

--- Controllers/bankAccounts.js
+++ Controllers/bankAccounts.js
@@ -1,6 +1,7 @@
 const BankAccount = require('../Models/BankAccount');
 const Customer = require('../Models/Customer');
 const Advisor = require('../Models/Advisor');
+const Manager = require('../Models/Manager');
 
 
 const findInTheDatabase = (customer_id, _type) => {
@@ -73,14 +74,46 @@ const getOneBankAccount = (req, res) => {
         return res.status(400).send(error);
     })
 }
-
+/*
 const checkAdvisorId = (advisor_id, customer_id) => {
     return Customer.findOne({advisorId: advisor_id, _id: customer_id})
-    .then((advisorId) => {
-        console.log(advisorId);
-        if(advisorId) return true;
+    .then((customer) => {
+        console.log(customer);
+        if(customer) return true;
         else return false;
     })
+}
+*/
+const checkManagerId = async (manager_id, account) => {
+    let result = 0;
+    let validate = false;
+    let customersFound = [];
+    let bankAccountsFound = [];
+    let advisorsFound = await Advisor.find({ managerId: manager_id });
+
+    for (let i = 0; i < advisorsFound.length; i++) {
+        const element = advisorsFound[i];
+        let customer = await Customer.find({ advisorId: element._id });
+        customersFound.push(customer);
+    }
+
+    for (let i = 0; i < customersFound.length; i++) {
+        const element = customersFound[i];
+        for (let index = 0; index < element.length; index++) {
+            const el = element[index];
+            let bankAccount = await BankAccount.find({ customerId: el._id });
+            bankAccountsFound.push(bankAccount);
+        }
+    }
+    bankAccountsFound.forEach(element => {
+        element.forEach(el => {
+            if (account._id.toString() === el._id.toString()) {
+                result += 1;
+                if (result === 1) validate = true;
+            }
+        })
+    })
+    return validate;
 }
 
 const putBankAccount = async (req, res) => {
@@ -90,16 +123,16 @@ const putBankAccount = async (req, res) => {
     const account = await BankAccount.findById(id);
     const customer = await Customer.findById(userId);
     const advisor = await Advisor.findById(userId);
+    const manager = await Manager.findById(userId);
     let checkCustomer;
     let checkAdvisor;
-    //On verifie que le user est un customer et que c'est le propriétaire du compte
+    //On verifie si le user est un customer et que c'est le propriétaire du compte
     if (customer != null || customer != undefined) {
         if (account.customerId.toString() === customer._id.toString()) {
             checkCustomer = true;
         }
     }
-
-    //On verifie que le user est un advisor et qu'il s'agit bien de l'advisor du customer de ce bankAccount
+    //On verifie si le user est un advisor et qu'il s'agit bien de l'advisor du customer de ce compte
     if (advisor != null || advisor != undefined) {
         try {
             checkAdvisor = await checkAdvisorId(advisor._id, account.customerId);
@@ -107,8 +140,16 @@ const putBankAccount = async (req, res) => {
             return res.status(500).send(error);
         }
     }
-    
-    if (checkCustomer === true || checkAdvisor === true) {
+    //On verifie si le user est un manager et qu'il s'agit bien du manager de l'advisor du customer de ce compte
+    if (manager != null || manager != undefined) {
+        try{
+            checkManager = await checkManagerId(manager._id, account);
+        } catch (error) {
+            return res.status(500).send(error);
+        }
+    }
+    //Si le user est lié à ce compte alors on fait la modif
+    if (checkCustomer === true || checkAdvisor === true || checkManager === true) {
         return BankAccount.findByIdAndUpdate(id, { amount: amount })
         .then((amountUpdated) => {
             return res.status(201).send(amountUpdated);
@@ -120,25 +161,6 @@ const putBankAccount = async (req, res) => {
         return res.status(403).send('You don\'t have access to this bank account\'s data !');
     }
 }
-
-
-/*
-const putBankAccount = async (req, res) => {
-    const id = req.params.id;
-    const userId = req.headers.userid;
-    const amount = req.body.amount;
-    const account = await BankAccount.findById(id);
-    const customer = await Customer.findById(userId);
-    const advisor = await Advisor.findById(userId);
-    let result;
-    try {
-        result = await checkAdvisorId(advisor._id, account.customerId);
-    } catch (error) {
-        return error;
-    }
-    console.log(result);
-}
-*/
 
 const deleteBankAccount = (req, res) => {
     const id = req.params.id;
