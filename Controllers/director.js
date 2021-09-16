da76847b1920f01c14e1aae38d8fe49094cd163f
Fadel Seck <fadelseck92@gmail.com>
2021-09-16 18:06:56 +0200

    ajout des autorisation pour la function getDirector du controller director.js

--- Controllers/director.js
+++ Controllers/director.js
@@ -28,15 +28,28 @@ const createDirector = async (req, res) => {
     }
 }
 
-const getDirector = (req, res) => {
-    return Director.find()
-    .then((directorFound) => {
-        return res.send(directorFound);
-    })
-    .catch((error) => {
-        return res.status(400).send(error);
-    })
+const getDirector = async (req, res) => {
+    const userId = req.headers.userid;
+    const isValidUser = mongoose.isValidObjectId(userId);
+    if (isValidUser) {
+        const director = await Director.findById(userId);
+        if (director) {
+            return Director.find()
+            .then((directorFound) => {
+                return res.send(directorFound);
+            })
+            .catch((error) => {
+                return res.status(400).send(error);
+            })
+        } else {
+            return res.status(403).send('You don\'t have access to the director data !');
+        }
+    } else {
+        return res.status(400).send("le userId saisi n'est pas valide !");
+    }
 }
+
+
 
 const deleteDirector = (req, res) => {
     const id = req.params.id;
